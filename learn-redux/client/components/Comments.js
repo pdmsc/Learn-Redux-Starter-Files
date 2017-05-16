27cf88f882261d7a77f812f06d3df79f04fd164e
pdmsc <pdm_sc@hotmail.com>
2017-05-16 00:57:28 -0700

    video13 - new code for the single component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
+
+const Comments = React.createClass({
+    
+    render() {
+        
+        return ( 
+           <div>COMMENTS</div>
+
+        )
+
+        
+    }
+
+
+});
+
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
-    render() {
-        return(
-            <div className="single-photo">
-                Single
-            
-            </div>        
+  render() {
+    //this comes from the URL  
+    const { postId } = this.props.params;
 
+    //Search the post index using postId in the posts array
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    //gets the actual post
+    const post = this.props.posts[i];
 
+    //gets the comments for the post
+    const postComments = this.props.comments[postId] || [];
 
-        )
-    }
-
-
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
+      </div>
+    )
+  }
 });
 
 export default Single;
